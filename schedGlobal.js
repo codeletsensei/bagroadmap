3d1db8feb6beee16e47df6f1173ba620e589be22
anon <nonomail@nonomail.com>
2025-04-19 22:27:41 -0300

    Update schedGlobal.js

--- schedGlobal.js
+++ schedGlobal.js
@@ -1433,6 +1433,13 @@ confirmed = [
     "subgroup": "JFD",
     "style": "color:white; background-color:red "
   },{
+    "content": "[GA] Chesed Outdoors (Blue Tor + Yellow/Purple) (Red atk)",
+    "start": "2025-05-06T02:00:00.000Z",
+    "end": "2025-05-12T19:00:00.000Z",
+    "group": "G Raid",
+    "subgroup": "GA",
+    "style": "color:white; background-color:blue "
+  },{
     "content": "[LBA] Chokmah Blue (Blue atk)",
     "start": "2025-04-16T02:00:00.000Z",
     "end": "2025-05-12T19:00:00.000Z",
