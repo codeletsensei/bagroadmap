5a342b82266c433ec18fb3015d206cb1e660f24c
anon <nonomail@nonomail.com>
2025-05-09 11:24:22 -0300

    Update schedGlobal.js

--- schedGlobal.js
+++ schedGlobal.js
@@ -181,6 +181,13 @@ confirmed = [
     "group": "G Maint",
     "subgroup": "",
     "style": "color:red; background-color:red; height:15px"
+  },{
+    "content": "EOS",
+    "start": "2025-05-13T02:00:00.000Z",
+    "end": "2025-05-13T10:00:00.000Z",
+    "group": "G Maint",
+    "subgroup": "",
+    "style": "color:red; background-color:red; height:15px"
   },
 //
 // GASHA
