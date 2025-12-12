354d14ff82f627fce696ab8ca56644bf66e16018
anon <nonomail@nonomail.com>
2025-12-12 12:40:51 -0300

    Update schedGlobal.js

--- schedGlobal.js
+++ schedGlobal.js
@@ -287,6 +287,13 @@ confirmed = [
     "subgroup": "",
     "style": "color:red; background-color:red; height:15px"
   },{
+    "content": "EOS",
+    "start": "2025-12-16T02:00:00.000Z",
+    "end": "2025-12-16T07:00:00.000Z",
+    "group": "G Maint",
+    "subgroup": "",
+    "style": "color:red; background-color:red; height:15px"
+  },{
 //
 // GASHA
 //
