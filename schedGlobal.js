ac07385ea6709197cbcec157a350a9d018f70fa7
anon <nonomail@nonomail.com>
2026-03-31 13:17:19 -0300

    Update schedGlobal.js

--- schedGlobal.js
+++ schedGlobal.js
@@ -4530,6 +4530,62 @@ confirmed = [
     "subgroup": "Campaign",
     "style": "color:black; background-color:lightgreen "
   },{
+    "content": "2x Bounty",
+    "start": "2026-04-06T19:00:00.000Z",
+    "end": "2026-04-13T19:00:00.000Z",
+    "group": "G Camp",
+    "subgroup": "Campaign",
+    "style": "color:black; background-color:lightgreen "
+  },{
+    "content": "2x Normal",
+    "start": "2026-04-13T19:00:00.000Z",
+    "end": "2026-04-20T19:00:00.000Z",
+    "group": "G Camp",
+    "subgroup": "Campaign",
+    "style": "color:black; background-color:lightgreen "
+  },{
+    "content": "2x Hard",
+    "start": "2026-04-13T19:00:00.000Z",
+    "end": "2026-04-20T19:00:00.000Z",
+    "group": "G Camp",
+    "subgroup": "Campaign",
+    "style": "color:black; background-color:lightgreen "
+  },{
+    "content": "2x Scrimm",
+    "start": "2026-04-20T19:00:00.000Z",
+    "end": "2026-04-27T19:00:00.000Z",
+    "group": "G Camp",
+    "subgroup": "Campaign",
+    "style": "color:black; background-color:lightgreen "
+  },{
+    "content": "2x Lesson",
+    "start": "2026-04-20T19:00:00.000Z",
+    "end": "2026-04-27T19:00:00.000Z",
+    "group": "G Camp",
+    "subgroup": "Campaign",
+    "style": "color:black; background-color:lightgreen "
+  },{
+    "content": "2x Bounty",
+    "start": "2026-04-27T19:00:00.000Z",
+    "end": "2026-05-04T19:00:00.000Z",
+    "group": "G Camp",
+    "subgroup": "Campaign",
+    "style": "color:black; background-color:lightgreen "
+  },{
+    "content": "<b>2x XP</b>",
+    "start": "2026-04-10T19:00:00.000Z",
+    "end": "2026-04-12T19:00:00.000Z",
+    "group": "G Camp",
+    "subgroup": "Campaign",
+    "style": "color:black; background-color:lightgreen "
+  },{
+    "content": "<b>2x XP</b>",
+    "start": "2026-04-17T19:00:00.000Z",
+    "end": "2026-04-19T19:00:00.000Z",
+    "group": "G Camp",
+    "subgroup": "Campaign",
+    "style": "color:black; background-color:lightgreen "
+  },{
 //
 /// MISC
 //
